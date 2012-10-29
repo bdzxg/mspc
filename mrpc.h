b9aabd1fa2e72f475be5a0c2e2a20328c89cc06d
llj098 <llj098@gmail.com>
2012-10-30 02:04:34 +0800

    move the rpc relative code to mrpc.h

---
+++ mrpc.h
@@ -0,0 +1,53 @@
+
+#ifndef __MRPC_H
+#define __MRPC_H
+
+#include <string.h>
+#include "proxy.h"
+#include "include/rpc_client.h"
+
+static uint rpc_upstreamer_sequence;
+rpc_upstreamer_t upstreamer = { 0 }; 
+
+typedef struct upstream_map_s {
+	struct rb_root root;
+	size_t count;
+} upstream_map_t;
+
+typedef struct upstreamer_send_queue_s {
+	list_head_t head;
+	void *data;
+} upstreamer_send_queue_t;
+
+typedef struct rpc_request_header_s {
+} rpc_request_header_t;
+
+typedef struct rpc_response_header_s {
+} rpc_response_header_t;
+
+typedef struct mrpc_message_s {
+	int mask;
+	int package_length;
+	short header_length;
+	short packet_options;
+	union h {
+		rpc_request_header_t req_head;
+		rpc_response_header_t resp_head;
+	};
+	short is_resp;
+	slice body;
+}mrpc_message_t;
+
+typedef struct mrpc_connection_s {
+	int fd;
+	struct buffer_s *send_buf;
+	struct buffer_s *recv_buf;
+	mrpc_message_t req;
+}mmrpc_connection_t;
+
+int mmrpc_upstreamer_init();
+int mmrpc_upstreamer_start();
+int mmrpc_upstreamer_send(upstream_t*, mmrpc_upstreamer_request_t*);
+int mmrpc_send_response(mmrpc_connection_t*, mmrpc_message_t*);
+
+#endif
