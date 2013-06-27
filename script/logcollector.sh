15a678017a72e5588935950002ad69c1643d7094
qinyuchun <qinyuchun@feinno.com>
2013-06-27 17:55:40 +0800

    modify log sh

--- script/logcollector.sh
+++ script/logcollector.sh
@@ -4,19 +4,19 @@ cd /data/mspclog1
 
 tar zcvf mspc`date -d last-day +%Y%m%d`.tar.gz  mspc*`date -d last-day +%Y%m%d_*`.log
 rm -f mspc*`date -d last-day +%Y%m%d_*`.log
-rm -f mspc`date -d '7 days ago' +%Y%m%d`.tar.gz
+rm -f mspc`date -d '3 days ago' +%Y%m%d`.tar.gz
 
 tar zcvf route`date -d last-day +%Y%m%d`.tar.gz  route*`date -d last-day +%Y%m%d_*`.log
 rm -f route*`date -d last-day +%Y%m%d_*`.log
-rm -f route`date -d '7 days ago' +%Y%m%d`.tar.gz
+rm -f route`date -d '3 days ago' +%Y%m%d`.tar.gz
 
 
 cd /data/mspclog2
 
 tar zcvf mspc`date -d last-day +%Y%m%d`.tar.gz  mspc*`date -d last-day +%Y%m%d_*`.log
 rm -f mspc*`date -d last-day +%Y%m%d_*`.log
-rm -f mspc`date -d '7 days ago' +%Y%m%d`.tar.gz
+rm -f mspc`date -d '3 days ago' +%Y%m%d`.tar.gz
 
 tar zcvf route`date -d last-day +%Y%m%d`.tar.gz  route*`date -d last-day +%Y%m%d_*`.log
 rm -f route*`date -d last-day +%Y%m%d_*`.log
-rm -f route`date -d '7 days ago' +%Y%m%d`.tar.gz
+rm -f route`date -d '3 days ago' +%Y%m%d`.tar.gz
